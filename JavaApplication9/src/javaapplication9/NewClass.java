6d0c50597c31b8024f9b78e2d092be60309f2725
xoxokhelz <xoxokel27@gmail.com>
2025-10-07 15:45:06 +0800

    adding new class and some changes in JavaApplication9

--- JavaApplication9/src/javaapplication9/NewClass.java
+++ JavaApplication9/src/javaapplication9/NewClass.java
@@ -3,6 +3,7 @@ package javaapplication9;
 
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("asdadsa");
+        System.out.println("asdads");
+        System.out.println("asdasd");
     }
 }
