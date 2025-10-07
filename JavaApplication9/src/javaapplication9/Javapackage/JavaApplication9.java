d6d923134fd3686f87755a8d363e7999facb681c
xoxokhelz <xoxokel27@gmail.com>
2025-10-07 19:26:44 +0800

    modified the sout in main

--- JavaApplication9/src/javaapplication9/Javapackage/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/Javapackage/JavaApplication9.java
@@ -1,9 +1,10 @@
 
-package Javapackage;
+package javaapplication9.Javapackage;
 
 public class JavaApplication9 {
 
     public static void main(String[] args) {
         System.out.println("Testing");
+        System.out.println("Testing again");
     }
 }
