7c5580a205611e8ef1904b7a87db3a195ae81257
xoxokhelz <128655043+xoxokel27@users.noreply.github.com>
2025-10-07 15:42:01 +0800

    Delete JavaApplication9 directory

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++
@@ -1,9 +0,0 @@
-
-package javaapplication9;
-
-public class JavaApplication9 {
-
-    public static void main(String[] args) {
-        System.out.println("Testing");
-    }
-}
--- JavaApplication9/src/javaapplication9/NewClass.java
+++
@@ -1,8 +0,0 @@
-
-package javaapplication9;
-
-public class NewClass {
-    public static void main(String[] args) {
-        System.out.println("asdadsa");
-    }
-}
