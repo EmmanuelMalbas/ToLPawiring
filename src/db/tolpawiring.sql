225edfba3671cc324784a564a5cb238d1703f412
xoxokhelz <xoxokel27@gmail.com>
2025-10-09 12:27:17 +0800

    - Column accoun_created Added

--- src/db/tolpawiring.sql
+++ src/db/tolpawiring.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2025 at 04:14 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.28
+-- Generation Time: Oct 09, 2025 at 06:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,19 +33,20 @@ CREATE TABLE `tbl_users` (
   `email` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` varchar(20) DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` varchar(20) DEFAULT 'user',
+  `account_created` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`id`, `flname`, `email`, `username`, `password`, `role`) VALUES
-(1, 'Emmanuel Malbas', '69.digong@gmail.com', 'shifter', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'admin'),
-(2, 'Kelly Domingo', '69.digong@gmail.com', 'toji', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'admin'),
-(3, 'John Doe', '69.digong@gmail.com', 'johndoe', 'e606e38b0d8c19b24cf0ee3808183162ea7cd63ff7912dbb22b5e803286b4446', 'user'),
-(4, 'Michael Jordan', 'mj@gmail.com', 'michael', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'user'),
-(5, '', 'asas', 'asas', 'b323ba8eb5a4df2f162b251ea20b37f6887c7f88714de8ab70ebb4dc99beade5', 'user');
+INSERT INTO `tbl_users` (`id`, `flname`, `email`, `username`, `password`, `role`, `account_created`) VALUES
+(1, 'Emmanuel Malbas', '69.digong@gmail.com', 'shifter', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'admin', '2025-10-09'),
+(2, 'Kelly Domingo', '69.digong@gmail.com', 'toji', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'admin', '2025-10-09'),
+(3, 'John Doe', '69.digong@gmail.com', 'johndoe', 'e606e38b0d8c19b24cf0ee3808183162ea7cd63ff7912dbb22b5e803286b4446', 'user', '2025-10-09'),
+(4, 'Michael Jordan', 'mj@gmail.com', 'michael', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'user', '2025-10-09'),
+(5, '', 'asas', 'asas', 'b323ba8eb5a4df2f162b251ea20b37f6887c7f88714de8ab70ebb4dc99beade5', 'user', '2025-10-09');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +67,7 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
